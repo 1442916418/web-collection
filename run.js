cf5c92951e90d65955f2a60e4350bdb692b63b53
yushuisheng <1442916418@qq.com>
2023-02-08 21:01:00 +0800

    update run.js...

--- run.js
+++ run.js
@@ -2,6 +2,12 @@ const fs = require('fs')
 const path = require('path')
 
 class Utils {
+  /**
+   * Open and read file
+   * @param {String} filePath path
+   * @param {callback} callback callback function
+   * @returns
+   */
   static handleOpenAndReadFile(filePath, callback) {
     if (!filePath) return
 
@@ -18,6 +24,12 @@ class Utils {
     })
   }
 
+  /**
+   * Write file
+   * @param {Object} v params
+   * @param {String} v.path path
+   * @param {String} v.content content
+   */
   static handleWriteFile(v) {
     if (!v.path) {
       throw new Error('\n utils handleWriteFile: 写入文件请传入路径')
@@ -104,30 +116,112 @@ class Build {
 class NewComponent {
   constructor(name) {
     this.name = name
+    this.componentsTemplateCode = ''
+    this.scssComponentsTemplateCode = ''
+    this.scssMixinTemplateCode = ''
+
     this.init()
   }
 
   init() {
+    this.handleTemplate()
+    this.handleNewComponent()
+  }
+
+  handleNewComponent() {
     const name = this.name
-    // 1. 新建 `src/components/${name}.js` 并写入默认代码
-    // 2. 新建 `src/components/styles.js` 并写入默认代码，正则匹配替换代码片段变量
+    const newName = name.toLocaleLowerCase()
+
+    // 1. 新建 `src/components/${name}/${name}.js` 并写入默认代码
+    Utils.handleWriteFile({
+      path: `src/components/${newName}/${newName}.js`,
+      content: this.componentsTemplateCode
+    })
+    // 2. 新建 `src/components/${name}/styles.js` 并写入默认代码，正则匹配替换代码片段变量
+    Utils.handleWriteFile({
+      path: `src/components/${newName}/styles.js`,
+      content: 'export default ``'
+    })
     // 3. 新建 `src/scss/neumorphism/components/_${name}.scss` 并写入默认代码，正则匹配替换代码片段变量
-    // 4. 新建 `src/scss/neumorphism/mixin/_${name}.scss` 并写入默认代码，正则匹配替换代码片段变量
+    Utils.handleWriteFile({
+      path: `src/scss/neumorphism/components/_${newName}.scss`,
+      content: this.scssComponentsTemplateCode
+    })
+    // 4. 新建 `src/scss/neumorphism/mixins/_${name}.scss` 并写入默认代码，正则匹配替换代码片段变量
+    Utils.handleWriteFile({
+      path: `src/scss/neumorphism/mixins/_${newName}.scss`,
+      content: this.scssMixinTemplateCode
+    })
     // 5. `src\scss\neumorphism\_components.scss` 追加新建文件路径
-    // 6. `src\scss\neumorphism\_mixin.scss` 追加新建文件路径
+    Utils.handleOpenAndReadFile('src/scss/neumorphism/_components.scss', (data) => {
+      Utils.handleWriteFile({
+        path: 'src/scss/neumorphism/_components.scss',
+        content: `${data};\n@import 'components/${newName}';`
+      })
+    })
+    // 6. `src\scss\neumorphism\_mixins.scss` 追加新建文件路径1
+    Utils.handleOpenAndReadFile('src/scss/neumorphism/_mixins.scss', (data) => {
+      Utils.handleWriteFile({
+        path: 'src/scss/neumorphism/_mixins.scss',
+        content: `${data};\n@import 'mixins/${newName}';`
+      })
+    })
+  }
+
+  handleTemplate() {
+    Utils.handleOpenAndReadFile('.vscode/template.code-snippets', (code) => {
+      if (code) {
+        const data = JSON.parse(code)
+
+        this.componentsTemplateCode = this.handleComponentsTemplateCode(data['Components template'])
+        this.scssComponentsTemplateCode = this.handleScssComponentsTemplateCode(data['SCSS Components template'])
+        this.scssMixinTemplateCode = this.handleScssMixinTemplateCode(data['SCSS Mixin template'])
+      }
+    })
+  }
+
+  handleComponentsTemplateCode(snippets) {
+    if (!snippets) return ''
+
+    let data = snippets.body.join(' ')
+
+    data = data
+      .replace(/\$1/gi, `Y${this.name}`)
+      .replace(/\$2/gi, `y-${this.name.toLocaleLowerCase()}`)
+      .replace(/\$0/gi, '')
+
+    return data
+  }
+
+  handleScssComponentsTemplateCode(snippets) {
+    if (!snippets) return ''
+
+    let data = snippets.body.join(' ')
+
+    data = data.replace(/\\\$/gi, '').replace(/\$1/gi, this.name.toLocaleLowerCase()).replace(/\$0/gi, '')
+
+    return data
+  }
+
+  handleScssMixinTemplateCode(snippets) {
+    if (!snippets) return ''
+
+    let data = snippets.body.join(' ')
+
+    data = data.replace(/\\$/gi, '').replace(/\$1/gi, this.name.toLocaleLowerCase()).replace(/\$0/gi, '')
+
+    return data
   }
 }
 
 const runType = process.env.RUN_TYPE
-const name = process.env.RUN_TYPE_NAME
+const name = process.env.FILE_NAME
 
 console.log('🚀 ~ file:', runType, name)
 
-// Utils.handleOpenAndReadFile('./.vscode/template.code-snippets', (code) => {})
-
-// if (runType === 'BUILD') {
-//   new Build()
-// }
-// if (runType === 'NEW' && name) {
-//   new NewComponent(name)
-// }
+if (runType === 'BUILD') {
+  new Build()
+}
+if (runType === 'NEW' && name) {
+  new NewComponent(name)
+}
